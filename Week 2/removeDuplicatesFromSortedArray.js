5c1eb19a51f213e37cfc44b475e363427082f781
Jesus Guadiana <jisus130@hotmail.com>
2019-02-25 15:03:51 -0600

    Moved removeDuplicatesFromSortedArray to week 3

--- Week 2/removeDuplicatesFromSortedArray.js
+++
@@ -1,8 +0,0 @@
-var removeDuplicates = function(nums) {  
-    for(var i = 1; i < nums.length; i++){
-        if(nums[i] == nums[i - 1]){
-            nums.splice(i, 1);
-            i--;
-        }
-    }
-};
