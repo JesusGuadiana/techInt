e96c12fed315fbabce4239a0759aceed6d618a6a
Jesus Guadiana <jisus130@hotmail.com>
2019-03-12 19:36:19 -0600

    two sum

---
+++ Daily Coding Problem/twoSum.js
@@ -0,0 +1,22 @@
+//DAY 1
+//Given a lis of numvers and a numverk , return wherher any two numbers from the list add up to k
+//Example [10, 15, 3, 7], k = 17 returns true;
+
+
+function twoSum(arr, k){
+  var l = 0;
+  var r = arr.length - 1;
+
+  arr.sort((a, b) => a - b);
+
+  while(l < r){
+    if(arr[l] + arr[r] == k){
+      return true;
+    } else if(arr[l] + arr[r] < k){
+      l++;
+    } else {
+      r--;
+    }
+  }
+  return false
+}
